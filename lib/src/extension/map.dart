45fbae624c6cd61e1c13442d4ecb61148fdb7896
devsdocs <73208025+devsdocs@users.noreply.github.com>
2023-08-29 23:28:24 +0900

    - Add `removeNullOrEmptyValue` on Map

--- lib/src/extension/map.dart
+++ lib/src/extension/map.dart
@@ -1,9 +1,25 @@
 part of '../reusable_tools_base.dart';
 
-extension MapExt on Map<String, dynamic> {
+extension MapOfStringDynamicExt on Map<String, dynamic> {
   String get toJsonString => json.encode(this);
   String get toJsonStringPretty {
     const encoder = JsonEncoder.withIndent('  ');
     return encoder.convert(this);
   }
+}
+
+extension MapExt on Map {
+  void get removeNullOrEmptyValue => removeWhere((_, v) {
+        if (v is List) {
+          if (v.isEmpty) return true;
+        }
+        if (v is Map) {
+          if (v.isEmpty) return true;
+        }
+        if (v is String) {
+          if (v.isEmpty) return true;
+        }
+        if (v == null) return true;
+        return false;
+      });
 }
